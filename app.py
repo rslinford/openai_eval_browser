a29a01c5b2a07c68c117f8cd64d0f2044634e78d
Scott <scott@linfords.com>
2023-05-12 16:38:40 -0700

    Display conversation in progress

--- app.py
+++ app.py
@@ -1,37 +1,42 @@
+import json
 import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+root_dir = '../evals/evals/registry/data'
 
-@app.route('/')
-def index():
-    # the directory to list
-    root_dir = '../evals/evals/registry/data'
-
+@app.route('/', methods=['GET'])
+def home():
     # get the subdirectories in the root directory
     sub_dirs = next(os.walk(root_dir))[1]
+    return render_template('home.html',
+                           sub_dirs=sub_dirs,
+                           samples=[],
+                           sample='')
 
-    return render_template('home.html', sub_dirs=sub_dirs)
+@app.route('/', methods=['POST'])
+def home_post():
+    # get the subdirectories in the root directory
+    sub_dirs = next(os.walk(root_dir))[1]
+    sub_dir = request.form['sub_dir']
+    if 'sample' in request.form:
+        x = request.form['sample']
+        sample = json.loads(x)
+    else:
+        sample = json.loads('{}')
+    samples_filename = 'samples.jsonl'
+    samples_full_filename = os.path.join(root_dir, sub_dir, samples_filename)
 
+    samples = []
+    with open(samples_full_filename, 'r') as file:
+        for line in file:
+            samples.append(line)
 
-@app.route('/list_files')
-def list_files():
-    # the root directory
-    root_dir = './static/files'
-
-    # get the selected subdirectory from the HTML form
-    selected_dir = request.args.get('selected_dir')
-
-    # if no subdirectory is selected, use the root directory
-    if not selected_dir:
-        selected_dir = root_dir
-
-    # get the files and subdirectories in the selected directory
-    files = os.listdir(selected_dir)
-    sub_dirs = [os.path.join(selected_dir, f) for f in files if os.path.isdir(os.path.join(selected_dir, f))]
-
-    return render_template('list_files.html', files=files, sub_dirs=sub_dirs)
+    return render_template('home.html',
+                           sub_dirs=sub_dirs,
+                           samples=samples,
+                           sample=sample)
 
 
 if __name__ == '__main__':
